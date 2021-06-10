de16276f5afe25a730de8d815ffc8a784f8e0474
francescozanini <zanini.francesco@hotmail.it>
2021-06-10 23:23:12 +0200

    transfer

--- tuto_7_graph.py
+++ tuto_7_graph.py
@@ -20,4 +20,19 @@ def create_graph(model_name):
             losses.append(float(loss))
             acc_vals.append(float(acc_val))
             loss_vals.append(float(loss_val))
-            
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((2, 1), (0, 0))
+    ax2 = plt.subplot2grid((2, 1), (1, 0), sharex=ax1)
+
+    ax1.plot(times, accs, label='accs')
+    ax1.plot(times, acc_vals, label='acc_vals')
+    ax1.legend(loc='best')
+
+    ax2.plot(times, losses, label='losses')
+    ax2.plot(times, loss_vals, label='loss_vals')
+    ax2.legend(loc='best')
+
+    plt.show()
+
+create_graph(model_name)
